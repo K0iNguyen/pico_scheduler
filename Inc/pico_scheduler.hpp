c2adf9cd3516fe80667cce972a759956e9846dde
K0iNguyen <khoinguyen070605@gmail.com>
2025-11-14 22:57:55 -0500

    Init

---
+++ Inc/pico_scheduler.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "pico/stdio.h"
+// #include "hardware/clocks.h"
+#include "hardware/timer.h"
+#include "hardware/ticks.h"
+
+
+class pico_scheduler{
+    public:
+    private:
+    protected:
+};
